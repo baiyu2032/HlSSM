a05fe228d5e829e74799e442c0532c30213a492c
baiyu2032 <1365402388@qq.com>
2018-11-01 12:30:40 +0800

    添加地址

---
+++ ssmdao/src/main/java/cn/dao/Madngeaddress.java
@@ -0,0 +1,15 @@
+package cn.dao;
+
+/**
+ * 功能描述: <br>
+ * 〈〉
+ *
+ * @return:地址管理
+ * @since: 1.0.0
+ * @Author:13654
+ * @Date: 2018/11/1 10:07
+ */
+public interface Madngeaddress {
+
+
+}
---
+++ ssmutil/src/main/java/cn/pojo/Address.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Address
+ * Author:   13654
+ * Date:     2018/11/1 11:30
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package cn.pojo;
+
+/**
+ * 功能描述: <br>
+ * 〈〉
+ *
+ * @param地址实体类
+ * @since: 1.0.0
+ * @Author:13654
+ * @Date: 2018/11/1 11:33
+ */
+public class Address {
+    /**
+     *地址id
+     */
+    private Integer aid;
+    /**
+     *联系人
+     */
+    private String aname;
+    /**
+     *手机号
+     */
+    private String phone;
+    /**
+     *邮政编码
+     */
+    private Integer postcode;
+    /**
+     *地址
+     */
+    private String local;
+    /**
+     *用户id
+     */
+    private Integer uid;
+
+
+    public Integer getAid() {
+        return this.aid;
+    }
+
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return this.aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getPostcode() {
+        return this.postcode;
+    }
+
+    public void setPostcode(Integer postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getLocal() {
+        return this.local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Integer getUid() {
+        return this.uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+}
