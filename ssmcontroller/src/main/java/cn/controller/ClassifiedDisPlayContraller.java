217b230bf4aebe5399f20d0b417fd3008a3b12f3
sjq19940827 <15803431215@163.com>
2018-11-09 09:08:40 +0800

    swagger配置了一下

--- ssmcontroller/src/main/java/cn/controller/ClassifiedDisPlayContraller.java
+++ ssmcontroller/src/main/java/cn/controller/ClassifiedDisPlayContraller.java
@@ -1,10 +1,7 @@
 package cn.controller;
 
 import cn.service.ClassifiedDisplayService;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiImplicitParam;
-import com.wordnik.swagger.annotations.ApiImplicitParams;
-import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,16 +28,14 @@ public class ClassifiedDisPlayContraller {
     @ResponseBody
     @RequestMapping(value = "showcom" ,method = RequestMethod.GET)
     @ApiOperation(value="按照ID查询相应商品",httpMethod="POST",notes="返回商品对象的json数据", response =String.class)
-    @ApiImplicitParam(name = "id",value = "电脑商品ID",dataType = "Integer",paramType = "path",required = true)
-    public String showcom(int id){
+    public String showcom(@ApiParam(name = "id",value = "电脑商品ID",required = true) int id){
         return cds.computerShow(id);
     }
 
     @ResponseBody
     @RequestMapping(value = "findbyname" ,produces = "application/json;charset=utf-8")
     @ApiOperation(value="模糊查询",httpMethod="POST",notes="返回查到的商品信息json数组", response =String.class)
-    @ApiImplicitParam(name = "s",value = "电脑名称",dataType = "String",paramType = "path",required = true)
-    public String findByComname(String s){
+    public String findByComname(@ApiParam(name = "s",value = "电脑名称",required = true) String s){
 //        SensitiveWordFilterConfig swfc=new SensitiveWordFilterConfig();
 //        String s1 = swfc.filterInfo(s);
         return cds.findByComname(s);
@@ -55,19 +50,14 @@ public class ClassifiedDisPlayContraller {
     @ResponseBody
     @RequestMapping("getallgoods")
     @ApiOperation(value = "分页查询所有商品",httpMethod = "POST",notes = "返回每页的商品", response =String.class)
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "index",value = "当前页码",paramType = "path",dataType = "Integer",required = true),
-            @ApiImplicitParam(name = "uid",value = "每页条数",paramType = "path",dataType = "Integer",required = true)
-    })
-    public String getallgoods(@RequestParam("index")int index, @RequestParam("page")int page){
+    public String getallgoods(@ApiParam(name = "index",value = "当前页码",required = true) @RequestParam("index")int index,@ApiParam(name = "uid",value = "每页条数",required = true) @RequestParam("page")int page){
         return cds.getallgoods(index,page);
     }
 
     @RequestMapping("getgoodsbyid")
     @ResponseBody
     @ApiOperation(value = "根据ID查询相应商品",httpMethod = "POST",notes = "返回相应商品对象", response =String.class)
-    @ApiImplicitParam(name = "gid",value = "电脑ID",dataType = "Integer",paramType = "path",required = true)
-    public String getgoodsbyid(@RequestParam("gid")int gid){
+    public String getgoodsbyid(@ApiParam(name = "gid",value = "电脑ID",required = true) @RequestParam("gid")int gid){
         return cds.getgoodsbyid(gid);
     }
 }
